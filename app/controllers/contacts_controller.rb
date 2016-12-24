3696ffa5973ac7d9ae244f5c9bb93ed79bf9985c
asardaes <alexis.sarda@gmail.com>
2016-12-24 16:44:32 +0100

    Added custom URL for contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,19 +2,19 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
   def create
     @contact = Contact.new(contact_params)
-    
+
     if @contact.save
       flash[:success] = "Message sent."
-      redirect_to new_contact_path
+      redirect_to contact_us_path
     else
       flash[:danger] = @contact.errors.full_messages.join(". ")
-      redirect_to new_contact_path
+      redirect_to contact_us_path
     end
   end
-  
+
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
